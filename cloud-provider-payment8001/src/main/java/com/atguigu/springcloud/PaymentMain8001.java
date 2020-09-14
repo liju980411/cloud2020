1959f9b069fd1a4db21cdfe6fbb311176597a791
liju980411 <1966317113@qq.com>
2020-09-14 14:31:34 +0800

    服务的发现：discovery

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -3,10 +3,12 @@ package com.atguigu.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.atguigu.springcloud.dao")
 public class PaymentMain8001 {
 
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -6,11 +6,15 @@ import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import sun.applet.Main;
 
 import javax.annotation.Resource;
+import java.util.List;
+
 //Spring4之后新加入的注解，原来返回json需要@ResponseBody和@Controller配合。
 //即@RestController是@ResponseBody和@Controller的组合注解。
 @RestController
@@ -21,6 +25,9 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private String serverPort;
@@ -49,4 +56,18 @@ public class PaymentController {
             return new CommonResult(444, "没有对应记录，查询id" + id, null);
         }
     }
+
+    @RequestMapping("/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for(String service:services){
+            log.info(service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for(ServiceInstance instance:instances){
+            log.info(instance.getServiceId()+instance.getHost()+instance.getInstanceId()+instance.getPort()+instance.getUri());
+        }
+        return this.discoveryClient;
+    }
+
 }
